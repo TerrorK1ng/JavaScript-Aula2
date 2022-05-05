0f6fec2562ae893416f63cfdb5b42c663035baa5
Claudir Santos <102737368+TerrorK1ng@users.noreply.github.com>
2022-05-05 10:51:00 -0300

    Add files via upload

---
+++ Atividade2.js
@@ -0,0 +1,33 @@
+let StringName = 'Claudir Pkrl';
+
+/*  Mostrar Nome    */
+function ShowName() {
+    console.log(StringName);
+    return
+}
+ShowName();
+
+ /* Fazer Soma*/
+function Somar(Numero1, Numero2) {
+    let ResultadoSoma = Numero1 + Numero2;
+    console.log('O Valor da soma foi: '+ResultadoSoma);
+    return
+}
+Somar(7,6);
+
+ /* Fazer Multiplicação*/
+function Multiplicar(Numero1, Numero2) {
+    let ResultadoMultiplicar = Numero1 * Numero2;
+    console.log('O Valor da soma foi: '+ResultadoMultiplicar);
+    return
+}
+Multiplicar(4,2);
+
+ /* Mostrar Média*/
+function Media(N1, N2, N3, N4) {
+    let ResultadoMedia = (N1+N2+N3+N4)/4;
+    let ResultadoTotal = ResultadoMedia/4
+    console.log('O Valor da Média Foi: ' +ResultadoMedia);
+    return
+}
+Media(2,4,5,8);
